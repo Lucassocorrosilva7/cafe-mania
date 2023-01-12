40c875821fb1325f6f042ac667f22fb8307d77ba
lukecode <lucassocorrosilva@gmail.com>
2023-01-12 16:44:02 -0300

    Add prisma

---
+++ prisma/data/categorys.ts
@@ -0,0 +1,31 @@
+
+const categorys = [
+    {
+        icon: "cafe",
+        name: "Café"
+      },
+      {
+        icon: "hamburger",
+        name: "Hamburgers"
+      },
+      {
+        icon: "pizza",
+        name: "Pizzas"
+      },
+      {
+        icon: "rosquinha",
+        name: "Rosquinhas"
+      },
+      {
+        icon: "pastel",
+        name: "Pastels"
+      },
+      {
+        icon: "biscoito",
+        name: "Biscoitos"
+      }
+]
+
+export {
+    categorys 
+}
