7bfed44cd422913c0c53f8398f614a9c596501ba
lukecode <lucassocorrosilva@gmail.com>
2023-01-15 19:54:24 -0300

    Criando context

--- context/coffeeProvider.jsx
+++ context/coffeeProvider.jsx
@@ -2,7 +2,11 @@ import { useState, useEffect, createContext } from "react";
 
 const CoffeeContext = createContext();
 
+
+
 const CoffeeProvider = ({ children }) => {
+const [coffe, setCoffe] = useState('');
+
   return <CoffeeContext.Provider value={{}}>{children}</CoffeeContext.Provider>;
 };
 
