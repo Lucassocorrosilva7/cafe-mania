99ea9781e3eaea40504393a47aaf8500279f1cff
lukecode <lucassocorrosilva@gmail.com>
2023-01-15 19:50:43 -0300

    Add provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+import { CoffeeProvider } from '../context/coffeeProvider'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CoffeeProvider>
+      <Component {...pageProps} />
+    </CoffeeProvider>
+  )
 }
