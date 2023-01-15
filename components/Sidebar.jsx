03158e170aec443c2e5b90839d44ef24cf3eef18
lukecode <lucassocorrosilva@gmail.com>
2023-01-15 19:55:39 -0300

    Criando componentes e componente de sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,9 +3,9 @@ import Logo from "../public/assets/img/logo.svg"
 
 const Sidebar = () => {
   return (
-    <>
+    <aside>
         <Image width={100} height={100} src={Logo} alt="Logotipo " priority />
-    </>
+    </aside>
   )
 }
 
