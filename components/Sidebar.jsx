1c5d07e74eb0491a64a0e883076d2b9cb65f4560
lukecode <lucassocorrosilva@gmail.com>
2023-01-16 18:07:20 -0300

    Consumindo dados da para mostrar icones na barra lateral

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,11 +1,22 @@
 import Image from 'next/image'
-import Logo from "../public/assets/img/logo.svg"
+import Logo from "../public/assets/images/logo.svg"
+import useCoffee from "../hooks/useCoffe";
+import Category from './Category';
 
 const Sidebar = () => {
+  const { categorys } = useCoffee();
   return (
-    <aside>
+    <>
         <Image width={100} height={100} src={Logo} alt="Logotipo " priority />
-    </aside>
+        <nav className='mt-10'>
+          {categorys.map(category => (
+            <Category 
+              key={category.id} 
+              category={category}
+            />
+          ))}
+        </nav>
+    </>
   )
 }
 
