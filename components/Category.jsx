6c86ecc40429651751ca8946fc3d79c40774be55
lukecode <lucassocorrosilva@gmail.com>
2023-01-16 18:11:08 -0300

    Criando componente de categoria para consumir api e importa para a barra lateral

---
+++ components/Category.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import useCoffee from "../hooks/useCoffe";
+
+const Category = ({ category }) => {
+  const { currentCategorys, handleClickcategory } = useCoffee();
+  const { name, icon, id } = category;
+
+  return (
+    <div
+      className={`${
+        currentCategorys?.id === id ? "bg-amber-400" : ""
+      } flex items-center gap-4 w-full border p-5 hover:bg-amber-400`}
+    >
+      <Image
+        width={70}
+        height={70}
+        src={`/assets/images/icon_${icon}.svg`}
+        alt="Imagem do icone"
+      />
+      <button
+        type="button"
+        className="text-2xl font-bold hover:cursor-pointer"
+        onClick={() => handleClickcategory(id)}
+      >
+        {name}
+      </button>
+    </div>
+  );
+};
+
+export default Category;
