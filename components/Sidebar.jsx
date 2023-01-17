c768198ff423368959a68f504d7bc58242923ba7
lukecode <lucassocorrosilva@gmail.com>
2023-01-17 20:54:05 -0300

    Passando novos estilos

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Logo from "../public/assets/images/logo.svg"
 import useCoffee from "../hooks/useCoffe";
 import Category from './Category';
 
@@ -7,14 +6,15 @@ const Sidebar = () => {
   const { categorys } = useCoffee();
   return (
     <>
-        <Image width={100} height={100} src={Logo} alt="Logotipo " priority />
-        <nav className='mt-10'>
+  <Image 
+        className='mt-5 m-auto'
+        width={100}
+        height={100}
+        src="/assets/images/logo.svg"
+        alt="imagen logotipo"
+      />        <nav className='mt-10'>
           {categorys.map(category => (
-            <Category 
-              key={category.id} 
-              category={category}
-            />
-          ))}
+            <Category key={category.id} category={category}/>))}
         </nav>
     </>
   )
