4a49a36c6682ec7ce41303f0b941cd6b12a4edb1
lukecode <lucassocorrosilva@gmail.com>
2023-01-19 17:03:58 -0300

    Incluindo produtos nas categorias

--- pages/api/category.js
+++ pages/api/category.js
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
 
-  const categorys = await prisma.category.findMany();
+  const categorys = await prisma.category.findMany({
+      include: {
+        products: true
+    }
+  });
 
   res.status(200).json(categorys);
 }
