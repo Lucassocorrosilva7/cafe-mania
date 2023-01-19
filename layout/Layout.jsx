a1f789a7703e2966ac559402f0c23074adb7a453
lukecode <lucassocorrosilva@gmail.com>
2023-01-19 17:03:22 -0300

    Passando novos estilos e add modal

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,22 +1,46 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
+import Modal from "react-modal";
+import useCoffee from "../hooks/useCoffe";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, page }) {
+  const { modal } = useCoffee();
+
   return (
     <>
       <Head>
-        <title>{`Café - ${page}`}</title>
+        <title>{page}</title>
         <meta name="description" content="Café mania" />
       </Head>
 
       <div className="md:flex">
-        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 ">
-         <Sidebar/>
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+          <Sidebar />
         </aside>
+
         <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          {children}
+          <div className="p-10">{children}</div>
         </main>
       </div>
+
+      {modal && (
+        <Modal isOpen={modal} style={customStyles}>
+          <h1>Modal...</h1>
+        </Modal>
+      )}
     </>
   );
 }
